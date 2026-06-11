a9c3887ee095a17190be0a6ae3db02696bfbccd0
Kamini Dokrimare <kamini@gmail.com>
2026-06-11 16:50:59 +0530

    first commit

---
+++ terraform/environments/dev/backend.hcl
@@ -0,0 +1,4 @@
+    bucket         = "demo-newbucket-terraform"
+    key            = "eks-platform/dev/terraform.tfstate"
+    region         = "eu-west-1"
+    encrypt        =  true
--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,9 +1,5 @@
 
 terraform {
   backend "s3" {
-    bucket         = "demo-newbucket-terraform"
-    key            = "eks-platform/dev/terraform.tfstate"
-    region         = "eu-west-1"
-    encrypt        = true
   }
 }
